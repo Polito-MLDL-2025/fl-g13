c79cf85df714a421afd164bdbad7cc201e0bb68e
nMaax <massimilianocarli@proton.me>
2025-04-24 21:39:31 +0200

    feat: updated model togekiss training with ImageNet mean and std normalization transform

---
+++ notebooks/script/3.01-cm-centralized-model-togekiss-nn.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
+
+
+import torch
+import torch.nn as nn
+from torch.nn import CrossEntropyLoss
+import torch.optim as optim
+from torch.optim import SGD
+from torch.optim.lr_scheduler import CosineAnnealingLR, CosineAnnealingWarmRestarts
+from torchvision import datasets
+from torch.utils.data import DataLoader
+
+from torchvision import models
+from torchvision.transforms import Compose, Resize, CenterCrop, RandomCrop, RandomHorizontalFlip, RandomVerticalFlip, Normalize, ToTensor
+
+from fl_g13.config import RAW_DATA_DIR
+from fl_g13.modeling import train, eval, save, load, backup, load_loss_and_accuracies, save_loss_and_accuracy
+from fl_g13.dataset import train_test_split
+
+from fl_g13.architectures import BaseDino
+
+
+# Define preprocessing pipeline
+train_transform = Compose([
+    Resize(256), # CIFRA100 is originally 32x32
+    RandomCrop(224), # But Dino works on 224x224
+    RandomHorizontalFlip(),
+    ToTensor(),
+    #Normalize(mean=[0.5071, 0.4866, 0.4409], std=[0.2673, 0.2564, 0.2762]),
+    Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])
+])
+
+eval_transform = Compose([
+    Resize(256), # CIFRA100 is originally 32x32
+    CenterCrop(224), # But Dino works on 224x224
+    ToTensor(),
+    #Normalize(mean=[0.5071, 0.4866, 0.4409], std=[0.2673, 0.2564, 0.2762]),
+    Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+cifar100_train = datasets.CIFAR100(root=RAW_DATA_DIR, train=True, download=True, transform=train_transform)
+cifar100_test = datasets.CIFAR100(root=RAW_DATA_DIR, train=False, download=True, transform=eval_transform)
+
+train_dataset, val_dataset = train_test_split(cifar100_train, 0.8, random_state=None)
+test_dataset = cifar100_test
+
+print(f"Train dataset size: {len(train_dataset)}")
+print(f"Validation dataset size: {len(val_dataset)}")
+print(f"Test dataset size: {len(test_dataset)}")
+
+
+# Move to CUDA
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
+# Settings
+CHECKPOINT_DIR = "/home/massimiliano/Projects/fl-g13/checkpoints"
+name = "togekiss-nn"
+start_epoch=1
+num_epochs=45
+save_every=5
+backup_every=10
+
+# Hyper-parameters
+BATCH_SIZE = 128
+LR = 1e-3
+
+# Dataloaders
+train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+val_dataloader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
+test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+# Model
+model = BaseDino(head_layers=5, head_hidden_size=512, dropout_rate=0.0, unfreeze_blocks=1)
+model.to(device)
+# Optimizer, scheduler, and loss function
+optimizer = SGD(model.parameters(), lr=LR)
+scheduler = CosineAnnealingWarmRestarts(
+    optimizer, 
+    T_0=15,           # First restart after 12 epochs
+    T_mult=2,        # Double the interval between restarts each time
+    eta_min=1e-6     # Minimum learning rate after annealing
+)
+criterion = CrossEntropyLoss()
+
+all_training_losses=[]       # Pre-allocated list for training losses
+all_validation_losses=[]       # Pre-allocated list for validation losses
+all_training_accuracies=[]    # Pre-allocated list for training accuracies
+all_validation_accuracies=[]    # Pre-allocated list for validation accuracies
+
+# # Model loading (uncomment to properly overwrite)
+# model, start_epoch = load(
+#     f"{CHECKPOINT_DIR}/BaseDino/togekiss-nn_BaseDino_epoch_x.pth",
+#     model_class=BaseDino,
+#     device=device,
+#     optimizer=optimizer,
+#     scheduler=scheduler,
+#     verbose=True
+# )
+# model.to(device)
+# loaded_metrics = load_loss_and_accuracies(path=f"{CHECKPOINT_DIR}/BaseDino/togekiss-nn_BaseDino_epoch_x.loss_acc.json")
+
+# Preallocated lists: if the training interrupts, it will still save their values
+# all_training_losses=loaded_metrics["train_loss"]       # Pre-allocated list for training losses
+# all_validation_losses=loaded_metrics["val_loss"]       # Pre-allocated list for validation losses
+# all_training_accuracies=loaded_metrics["train_acc"]    # Pre-allocated list for training accuracies
+# all_validation_accuracies=loaded_metrics["val_acc"]    # Pre-allocated list for validation accuracies
+
+print(f"\nModel: {model}")
+
+
+try:
+    _, _, _, _ = train(
+        checkpoint_dir=CHECKPOINT_DIR,
+        name=name,
+        start_epoch=start_epoch,
+        num_epochs=num_epochs,
+        save_every=save_every,
+        backup_every=backup_every,
+        train_dataloader=train_dataloader,
+        val_dataloader=val_dataloader,
+        model=model,
+        criterion=criterion,
+        optimizer=optimizer,
+        scheduler=scheduler,
+        verbose=False,
+        all_training_losses=all_training_losses,
+        all_validation_losses=all_validation_losses,
+        all_training_accuracies=all_training_accuracies,
+        all_validation_accuracies=all_validation_accuracies,
+    )
+
+except KeyboardInterrupt:
+    print("Training interrupted manually. Backing up latest checkpoint...")
+
+except Exception as e:
+    print(f"Training stopped due to error: {e}")
+
+
+import matplotlib.pyplot as plt
+
+# Plot losses
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.plot(all_training_losses, label='Training Loss')
+plt.plot(all_validation_losses, label='Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.title('Loss vs Epochs')
+plt.legend()
+
+# Plot accuracies
+plt.subplot(1, 2, 2)
+plt.plot(all_training_accuracies, label='Training Accuracy')
+plt.plot(all_validation_accuracies, label='Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.title('Accuracy vs Epochs')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
+
+# Evaluate the model on the validation dataset
+test_loss, test_accuracy, _ = eval(dataloader=test_dataloader, model=model, criterion=criterion)
+
+print(
+    f"🔍 Test Results:\n"
+    f"\t📉 Test Loss: {test_loss:.4f}\n"
+    f"\t🎯 Test Accuracy: {100 * test_accuracy:.2f}%"
+)
+
+
+
+
